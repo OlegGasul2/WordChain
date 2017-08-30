e1affc708a041504093f936f0cfb9d6bf1d41f78
OlegGasiul <oleg.gasiul@informa.com>
2017-08-30 16:52:11 +0200

    Initial commit

--- ChainSearcher/src/com/sabre/Main.java
+++ ChainSearcher/src/com/sabre/Main.java
@@ -1,12 +1,28 @@
 package com.sabre;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         WordChainProcessor processor = new WordChainProcessor();
-        processor.addWord("cat");
-        processor.addWord("can");
-        processor.addWord("pan");
+        processor.addWords("cat", "can", "pan", "tan");
+        processor.makeGraph();
+
+        printlnResult(processor.findRoute("cat", "tan"));
+
+    }
+
+    public static void printlnResult(List<Node> nodes) {
+        if (nodes == null) {
+            System.out.println("Route not found");
+            return;
+        }
+
+        for (Node node : nodes) {
+            System.out.print(node.value + ",");
+        }
+        System.out.println();
     }
 
 }
--- ChainSearcher/src/com/sabre/WordChainProcessor.java
+++ ChainSearcher/src/com/sabre/WordChainProcessor.java
@@ -1,12 +1,10 @@
 package com.sabre;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class WordChainProcessor {
     public Map<String, Set<Node>> possibilities = new HashMap<>();
+    private Map<String, Node> nodesMap = new HashMap<>();
 
     private void addPosibility(String key, Node value) {
         Set<Node> nodes = possibilities.get(key);
@@ -18,6 +16,12 @@ public class WordChainProcessor {
         nodes.add(value);
     }
 
+    public void addWords(String... args) {
+        for (String word : args) {
+            this.addWord(word);
+        }
+    }
+
     public void addWord(String word) {
         Node node = new Node(word);
 
@@ -27,5 +31,72 @@ public class WordChainProcessor {
             builder.append(word.substring(i + 1, word.length()));
             addPosibility(builder.toString(), node);
         }
+
+        nodesMap.put(word, node);
     }
+
+    private void addChildren(Node node) {
+        for (int i = 0; i < node.value.length(); i++) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(node.value.substring(0, i));
+            builder.append(node.value.substring(i + 1, node.value.length()));
+
+            Set<Node> list = possibilities.get(builder.toString());
+            if (list != null) {
+                for (Node elem : list) {
+                    if (!node.value.equals(elem.value)) {
+                        node.children.add(elem);
+                    }
+                }
+                node.children.addAll(list);
+            }
+        }
+    }
+
+    public void makeGraph() {
+        Collection<Node> nodes = nodesMap.values();
+        for (Node node : nodes) {
+            addChildren(node);
+        }
+    }
+
+    public List<Node> findRoute(String from, String to) {
+        Node node = nodesMap.get(from);
+        return new Router(this).findRoute(node, to);
+    }
+}
+
+class Router {
+    private WordChainProcessor processor;
+    private Set<Node> passed = new HashSet<>();
+
+    Router(WordChainProcessor processor) {
+        this.processor = processor;
+    }
+
+    public List<Node> findRoute(Node node, String to) {
+        passed.add(node);
+        List<Node> result = new LinkedList<>();
+
+        for (Node child : node.children) {
+            if (passed.contains(child)) {
+                continue;
+            }
+
+            if (child.value.equals(to)) {
+                result.add(node);
+                result.add(child);
+                return result;
+            }
+
+            List found = findRoute(child, to);
+            if (found != null) {
+                result.addAll(found);
+                return result;
+            }
+        }
+
+        return null;
+    }
+
 }
